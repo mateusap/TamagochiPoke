3c559050f07d24833b0dc86fcdb7f35c81467f23
Mateus Silva <ma.aparecido.silva@hotmail.com>
2022-09-27 10:48:01 -0300

    Inicio de projeto de um mascote virtual consumindo REST API

---
+++ TamagochiPokeAPI/Program.cs
@@ -0,0 +1,8 @@
+﻿using TamagochiPokeAPI;
+using TamagochiPokeAPI.Models;
+
+Console.WriteLine();
+var nome = Console.ReadLine();
+Pokemon pokemon = new();
+pokemon = PokemonService.BuscarPorEspecie(nome);
+Console.WriteLine(pokemon) ;
